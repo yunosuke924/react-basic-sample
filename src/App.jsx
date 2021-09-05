1a5c18b14654051fe1f416942cca4943e2466e42
Yunosuke Minegishi <72934392+yunosuke924@users.noreply.github.com>
2021-09-06 04:44:21 +0900

    Finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import {useEffect, useState} from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
-const App = () => {
-  const onClickButton = () => alert("test");
 
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const[faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[num]);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickShowFlag}>ON・OFF</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(^_^)</p>} 
     </>
   );
 };

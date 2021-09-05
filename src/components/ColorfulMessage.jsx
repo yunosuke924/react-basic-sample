1aa02f848556bbc682e68afc6afc0ecf0dc7671e
Yunosuke Minegishi <72934392+yunosuke924@users.noreply.github.com>
2021-09-06 00:23:04 +0900

    CSSの省略記法

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -2,7 +2,7 @@ const ColorfulMessage = (props) => {
   // 分割代入のテクニックを使う
   const { color, children } = props;
   const contentStyle = {
-    color: color,
+    color,
     fontSize: "18px"
   };
   return <p style={contentStyle}>{children}</p>;
